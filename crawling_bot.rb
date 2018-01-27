d0240a642cb738fe07d65797e81174f1006ce83a
Samuel Yemane Ayele <samzhab@gmail.com>
2018-01-27 02:04:17 +0100

    save core ranks to csv - part of pipeline for importing into OR

--- crawling_bot.rb
+++ crawling_bot.rb
@@ -28,61 +28,63 @@ require 'csv'
 # ----------------------------------------------------------------------------
 
 def start
-  csv_files = Dir.entries(Dir.pwd + '/csv_files/') # list of csv files
+  csv_files             = Dir.entries(Dir.pwd + '/csv_files/') # list of csv files
   puts '[START] started extracting all events to separate csv file'
-  core18_base = 'http://portal.core.edu.au/conf-ranks/?search='
-  core18_trail = '&by=all&source=CORE2018&sort=atitle&page=1'
-  core17_base = 'http://portal.core.edu.au/conf-ranks/?search='
-  core17_trail = '&by=all&source=CORE2017&sort=atitle&page=1'
+  core18_base           = 'http://portal.core.edu.au/conf-ranks/?search='
+  core18_trail          = '&by=all&source=CORE2018&sort=atitle&page=1'
+  core17_base           = 'http://portal.core.edu.au/conf-ranks/?search='
+  core17_trail          = '&by=all&source=CORE2017&sort=atitle&page=1'
   csv_files.each do |csv_file_name|
-    next if (csv_file_name == '.') || (csv_file_name == '..') # skip these
-    csv_file    = Dir.pwd + '/csv_files/' + csv_file_name
+    next if (csv_file_name == '.') || (csv_file_name == '..') || (csv_file_name == 'import.csv')# skip these
+    csv_file            = Dir.pwd + '/csv_files/' + csv_file_name
+    csv_rows            = []
     CSV.foreach(csv_file) do |row|
-      event_series = row[0].split(' ')[0]
+      event_series      = row[0].split(' ')[0]
       core17_fomred_url = core17_base + event_series + core17_trail
       core18_fomred_url = core18_base + event_series + core18_trail
-      core_2018_rank = crawl_core_17_ranking(core17_fomred_url)
-      core_2017_rank = crawl_core_18_ranking(core18_fomred_url)
-      if core_2017_rank
-        puts core_2017_rank
-        # puts core_2018_rank
-        puts '-/-/-//-/-/' + event_series
+      core_2018_rank    = crawl_core_17_ranking(core17_fomred_url)
+      core_2017_rank    = crawl_core_18_ranking(core18_fomred_url)
+        if core_2017_rank && core_2017_rank != 'Unranked'
+          puts 'found >>> ' + event_series
+          csv_rows << [event_series, core_2017_rank, core_2018_rank]
+          puts csv_rows
+        end
+    end
+    range               = 0..csv_rows.size - 1
+    CSV.open(Dir.pwd + '/csv_files/import.csv', 'wb') do |csv|
+      csv << ['Event[Series]', 'Event[has CORE2017 Rank]', 'Event[has CORE2018 Rank]']
+      range.each do |num|
+        csv << csv_rows[num]
       end
-      # do crawling for all csv files in folder here
     end
   end
 end
 
 def mock_crawl
   puts '[START] started extracting all events to separate csv file'
-  core18_base = 'http://portal.core.edu.au/conf-ranks/?search='
-  core18_trail = '&by=all&source=CORE2018&sort=atitle&page=1'
-  core17_base = 'http://portal.core.edu.au/conf-ranks/?search='
-  core17_trail = '&by=all&source=CORE2017&sort=atitle&page=1'
-  event_series = 'WWW'
-  # event_series = 'ICPRAI'
-  core17_fomred_url = core17_base + event_series + core17_trail
-  core18_fomred_url = core18_base + event_series + core18_trail
-  core_2018_rank = crawl_core_17_ranking(core17_fomred_url)
-  core_2017_rank = crawl_core_18_ranking(core18_fomred_url)
-  puts core_2017_rank
+  core18_base           = 'http://portal.core.edu.au/conf-ranks/?search='
+  core18_trail          = '&by=all&source=CORE2018&sort=atitle&page=1'
+  core17_base           = 'http://portal.core.edu.au/conf-ranks/?search='
+  core17_trail          = '&by=all&source=CORE2017&sort=atitle&page=1'
+  event_series          = 'WWW'
+  core17_fomred_url     = core17_base + event_series + core17_trail
+  core18_fomred_url     = core18_base + event_series + core18_trail
+  core_2018_rank        = crawl_core_17_ranking(core17_fomred_url)
+  core_2017_rank        = crawl_core_18_ranking(core18_fomred_url)
 end
 
 def crawl_core_18_ranking(core18_fomred_url)
   begin
-    response = RestClient::Request.execute(method: :get,
+    response            = RestClient::Request.execute(method: :get,
                                            url: Addressable::URI
                                              .parse(core18_fomred_url)
                                              .normalize.to_str,
                                            timeout: 5)
-    doc = Nokogiri::HTML(response)
-    rank_table = doc.at('table')
+    doc                 = Nokogiri::HTML(response)
+    rank_table          = doc.at('table')
     if rank_table
-      puts rank_table.search('td.nowrap').text.split(' ')[2]
-      core_2018_rank = rank_table.search('td.nowrap').text.split(' ')[2]
-      # table tbody tr.evenrow td.nowrap
+      core_2018_rank    = rank_table.search('td.nowrap').text.split(' ')[2]
     end
-#container.container_12.clearfix div.grid_12 div#search table
   rescue RestClient::ExceptionWithResponse => e
     puts e.response
   end
@@ -91,19 +93,16 @@ end
 
 def crawl_core_17_ranking(core17_fomred_url)
   begin
-    response = RestClient::Request.execute(method: :get,
+    response            = RestClient::Request.execute(method: :get,
                                            url: Addressable::URI
                                              .parse(core17_fomred_url)
                                              .normalize.to_str,
                                            timeout: 5)
-    doc = Nokogiri::HTML(response)
-    rank_table = doc.at('table')
+    doc                 = Nokogiri::HTML(response)
+    rank_table          = doc.at('table')
     if rank_table
-      # puts rank_table.search('tr.evenrow').text.split(' ')[7]
-      core_2017_rank = rank_table.search('td.nowrap').text.split(' ')[2]
-      # table tbody tr.evenrow td.nowrap
+      core_2017_rank    = rank_table.search('td.nowrap').text.split(' ')[2]
     end
-#container.container_12.clearfix div.grid_12 div#search table
   rescue RestClient::ExceptionWithResponse => e
     puts e.response
   end
@@ -111,8 +110,3 @@ def crawl_core_17_ranking(core17_fomred_url)
 end
 # mock_crawl
 start
-# ICPRAI
-# http://portal.core.edu.au/conf-ranks/1548/
-# http://portal.core.edu.au/conf-ranks/1548/
-# ICMLC
-# WWW
